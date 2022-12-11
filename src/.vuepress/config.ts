7f01bc24035b9695c9e8c82ab5431146206e142e
Minf97 <85478107+Minf97@users.noreply.github.com>
2022-12-12 00:25:39 +0800

    fix

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/Minf97/Minf-blog/",
+  base: "/",
 
   // base: "/github账号名称/网址名称/"
 
